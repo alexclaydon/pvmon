e0df7ed29341bc92e8a0ea3870aeb6dd5251ad40
Alex Claydon <alexclaydon@gmail.com>
2020-08-20 15:34:25 +0900

    Experimenting with pytest fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,24 @@
+import pytest
 from liblogger.legacy import local_logger
 import pandas as pd
-import pytest
+import random
+
+
+source_ecomegane_test_data = {
+    'project_id': random.sample(range(10, 30), 10),
+    'project_name': ['proj'] * 10,
+    'sensor_num': random.sample(range(10, 30), 10),
+    'sensor_id': random.sample(range(10, 30), 10),
+    'location': random.sample(range(10, 30), 10),
+    'observation_date': random.sample(range(10, 30), 10),
+    'total_prod_kwh': random.sample(range(10, 30), 10),
+    'total_sold_kwh': random.sample(range(10, 30), 10),
+    'sale_type': random.sample(range(10, 30), 10),
+}
 
 
 @pytest.fixture(scope='function')
-def example_fixture():
-    local_logger.info("Setting Up Example Fixture...")
-    yield
-    local_logger.info("Tearing Down Example Fixture...")
-
-
-
-
-dataframe = pd.DataFrame(
-    columns=['project_id', 'project_name', 'sensor_num', 'sensor_id', 'location', 'observation_date', 'total_prod_kwh', 'total_sold_kwh', 'sale_type'],
-)
+def ecomegane_test_data():
+    return pd.DataFrame(
+        data=source_ecomegane_test_data,
+    )
